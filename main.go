abf8865debd0174039bc6db778451363bb8e0b24
Ramy Aboul Naga <ramy.naga@gmail.com>
2018-10-29 09:54:23 +0200

    Add web server + docker-compose.yml

--- main.go
+++ main.go
@@ -4,30 +4,42 @@ import (
 	"database/sql"
 	"fmt"
 	"log"
+	"net/http"
+	"os"
 
 	_ "github.com/lib/pq"
 )
 
-const (
-	dbUser     = "root"
-	dbPassword = "secret"
-	dbName     = "myapp"
+var (
+	dbHost     = envOrDefault("MYAPP_DATABASE_HOST", "localhost")
+	dbPort     = envOrDefault("MYAPP_DATABASE_PORT", "5432")
+	dbUser     = envOrDefault("MYAPP_DATABASE_USER", "root")
+	dbPassword = envOrDefault("MYAPP_DATABASE_PASSWORD", "secret")
+	dbName     = envOrDefault("MYAPP_DATABASE_NAME", "myapp")
+
+	webHost = envOrDefault("MYAPP_WEB_HOST", "")
+	webPort = envOrDefault("MYAPP_WEB_PORT", "8080")
+
+	db *sql.DB
 )
 
-func main() {
-	dbInfo := fmt.Sprintf("user=%s password=%s dbname=%s sslmode=disable",
-		dbUser, dbPassword, dbName)
-	db, err := sql.Open("postgres", dbInfo)
-	if err != nil {
-		log.Fatal(err)
+func envOrDefault(key, defaultValue string) string {
+	if value := os.Getenv(key); value != "" {
+		return value
 	}
+	return defaultValue
+}
 
+func myHandler(w http.ResponseWriter, r *http.Request) {
 	rows, err := db.Query("SELECT * FROM users")
 	if err != nil {
-		log.Fatal(err)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
 	}
 	defer rows.Close()
 
+	fmt.Fprintln(w, "ID | Name")
+	fmt.Fprintln(w, "---+--------")
 	for rows.Next() {
 		var (
 			id   int
@@ -36,6 +48,23 @@ func main() {
 
 		rows.Scan(&id, &name)
 
-		log.Printf("%d: %s", id, name)
+		fmt.Fprintf(w, "%2d | %s\n", id, name)
 	}
+}
+
+func main() {
+	dbInfo := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
+		dbHost, dbPort, dbUser, dbPassword, dbName)
+	var err error
+	db, err = sql.Open("postgres", dbInfo)
+	if err != nil {
+		log.Fatal(err)
+	}
+	if err = db.Ping(); err != nil {
+		log.Fatal(err)
+	}
+
+	http.HandleFunc("/", myHandler)
+	log.Print("Listening on " + webHost + ":" + webPort + "...")
+	http.ListenAndServe(webHost+":"+webPort, nil)
 }
